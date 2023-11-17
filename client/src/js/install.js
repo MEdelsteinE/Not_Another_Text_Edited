4a637fdc22059faeff98e62e12444bcc2d0efb06
MedelsteinE <medelstein1123@gmail.com>
2023-11-17 18:09:51 -0500

    fixed butinstall event listener

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -10,11 +10,14 @@ butInstall.style.display = 'block';
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    if (!window.deferredPrompt) {
+        console.log('The install prompt is not available yet.');
+        return;
+    }
     butInstall.style.display = 'none';
     window.deferredPrompt.prompt();    
-    const { outcome } = await deferredPrompt.userChoice;
+    const { outcome } = await window.deferredPrompt.userChoice;
 });
-
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('App installed');
